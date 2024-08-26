0f6ac82cc5f9df1bb8a6e73de352e571afed6244
giuliana <giuliana.silva@outsystems.com>
2024-08-26 10:38:36 +0100

    Remove router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonLabel, IonTab, IonTabs, IonTabBar, IonTabButton, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonLabel, IonTab, IonTabs, IonTabBar, IonTabButton, IonIcon],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() { }
 }
